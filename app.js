f7cf1fc4e554ff47f5dc5eadf380e254710193e9
Garvit <138558861+GarvitMahhesh@users.noreply.github.com>
2025-01-12 12:53:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello , to check all the apis virtually go on https://backendapi-1bda.onrender.com/api/users/create');
 });
 const swaggerDefinition = {
     openapi: '3.0.0',
